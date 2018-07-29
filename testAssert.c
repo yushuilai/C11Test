75c95cbb8f7ea131f7af2238ff20a2a908f07c1f
yushuilai <yushuilai@koolearn.com>
2018-07-29 21:51:05 +0800

    1.modified assert test.

--- testAssert.c
+++ testAssert.c
@@ -1,30 +1,11 @@
 #include <assert.h>
 #include <stdio.h>
-#include <errno.h>
-#include <math.h>
-
-void show_errno(void){
-   
-   switch(errno){
-    case EDOM:
-        printf("Domain error \n");
-        break;
-    case EILSEQ:
-        printf("Illegal sequence!");
-        break;
-    case ERANGE:
-        printf("pole or range error!");
-        break;
-    default:
-        break;
-   }
-}
+//#define NDEBUG
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("MATH_ERRNO is %s\n", math_errhandling & MATH_ERRNO ? "set" : "not set");
-    errno = 0;
-    1.0/0.0;
+    int a = 1;
+    int b = 2;
 
-
+    assert(a + b > 3);
 }
